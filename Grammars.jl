9c503d5f2d79911d120d113f7c6c2d8cee3c986b
Michael Fox <m@antikc.com>
2013-12-01 08:30:09 -0800

    everything changes

--- Grammars.jl
+++ Grammars.jl
@@ -1,24 +1,24 @@
 
-module Grammar
+module Grammars
 
-export MyGrammar
+export Grammar
 
 import Expressions: Literal, Regex, Sequence, OneOf, Lookahead, Optional, ZeroOrMore, OneOrMore, Not, Expression, parse
 # NodeVisitor is abstract and it tells me I can't import it
-import Nodes: Node, NodeVisitor
+import Nodes: Node, NodeVisitor, pprettily
 
-type MyGrammar
+type Grammar
     rules::ASCIIString
     default_rule::ASCIIString
     exprs::Dict
 end
 
-function MyGrammar(rules::ASCIIString, default_rule=nothing)
+function Grammar(rules::ASCIIString, default_rule=nothing)
     exprs, first = _expressions_from_rules(rules)
     if isa(default_rule, Nothing)
         default_rule = first.name
     end
-    MyGrammar(rules, default_rule, exprs)
+    Grammar(rules, default_rule, exprs)
 end
 
 function _expressions_from_rules(rules::ASCIIString)
@@ -36,8 +36,12 @@ function _bootstrapping_expressions_from_rules()
     # TODO: I shouldn't have to provide the leading ^
     # TODO: Swap single and double quote syntax since Julia insists on
     # double quotes
-    rule_syntax = ""
-    more_rule_syntax = "
+    # I doubled up the \\ and in some cases (if not all) that's the wrong thing to do
+    # I don't want to use r"regex" syntax because that forces regex compiling and then I would have
+    # to recomile to prepend the ^. But that may be an okay short-term solution. Also, I can
+    # invent a different type of quote (or see if someone has) which preserves \ exactly so that
+    # '\s' doesn't turn into 's' which was causing me headaches
+    rule_syntax = """
         # Ignored things (represented by _) are typically hung off the end of the
         # leafmost kinds of nodes. Literals like '/' count as leaves.
 
@@ -47,8 +51,8 @@ function _bootstrapping_expressions_from_rules()
         literal = spaceless_literal _
 
         # So you can't spell a regex like `~'...' ilm`:
-        spaceless_literal = ~'u?r?\'[^\'\\\\]*(?:\\\\.[^\'\\\\]*)*\''is /
-                            ~'u?r?'[^'\\\\]*(?:\\\\.[^'\\\\]*)*''is
+        spaceless_literal = ~'u?r?\\'[^\\'\\\\\\\\]*(?:\\\\\\\\.[^\\'\\\\\\\\]*)*\\''is / 
+                            ~'u?r?\"[^"\\\\\\\\]*(?:\\\\\\\\.[^"\\\\\\\\]*)*\"'is
 
         expression = ored / sequence / term
         or_term = '/' _ term
@@ -62,6 +66,7 @@ function _bootstrapping_expressions_from_rules()
         regex = '~' spaceless_literal ~'[ilmsux]*'i _
         parenthesized = '(' _ expression ')' _
         quantifier = ~'[*+?]' _
+
         reference = label !equals
 
         # A subsequent equal sign is the only thing that distinguishes a label
@@ -69,19 +74,19 @@ function _bootstrapping_expressions_from_rules()
         # rule defined somewhere else):
         label = ~'[a-zA-Z_][a-zA-Z_0-9]*' _
 
-        # _ = ~r'\s*(?:#[^\r\n]*)?\s*'
+        # _ = ~r'\\s*(?:#[^\\r\\n]*)?\\s*'
         _ = meaninglessness*
-        meaninglessness = ~r'\s+' / comment
-        comment = ~r'#[^\r\n]*'
-        "
-    comment = Regex("^#[^\r\n]*", name="comment")
-    meaninglessness = OneOf(Regex("^\s+"), comment, name="meaninglessness")
+        meaninglessness = ~r'\\s+' / comment
+        comment = ~r'#[^\\r\\n]*'
+        """
+    comment = Regex("#[^\\r\\n]*", name="comment")
+    meaninglessness = OneOf(Regex("\\s+"), comment, name="meaninglessness")
     _ = ZeroOrMore(meaninglessness, name="_")
     equals = Sequence(Literal("="), _, name="equals")
-    label = Sequence(Regex("^[a-zA-Z_][a-zA-Z_0-9]*"), _, name="label")
+    label = Sequence(Regex("[a-zA-Z_][a-zA-Z_0-9]*"), _, name="label")
     reference = Sequence(label, Not(equals), name="reference")
-    quantifier = Sequence(Regex("^[*+?]"), _, name="quantifier")
-    spaceless_literal = Regex("^u?r?'[^'\]*(?:\\.[^'\]*)*'",
+    quantifier = Sequence(Regex("[*+?]"), _, name="quantifier")
+    spaceless_literal = Regex("u?r?'[^'\\\\]*(?:\\\\.[^'\\\\]*)*'",
         options="is",
         name="spaceless_literal")
     literal = Sequence(spaceless_literal, _, name="literal")
@@ -95,6 +100,7 @@ function _bootstrapping_expressions_from_rules()
 
     term = OneOf(quantified, atom, name="term")
     not_term = Sequence(Literal("!"), term, _, name="not_term")
+    unshift!(term.members, not_term)
 
     sequence = Sequence(term, OneOrMore(term), name="sequence")
     or_term = Sequence(Literal("/"), _, term, name="or_term")
@@ -103,15 +109,65 @@ function _bootstrapping_expressions_from_rules()
     rule = Sequence(label, equals, expression, name="rule")
     rules = Sequence(_, OneOrMore(rule), name="rules")
 
+    # Turn the parse tree into a map of expressions:
     rule_tree = parse(rules, rule_syntax)
+    println("LETS GO VISIT THE RULE TREE")
+    visit(RuleVisitor(), rule_tree)
+
+    # try_tree = parse(""")
 end
 
+type RuleVisitor <: NodeVisitor end
 
-# TODO: something like """ for quoting?
-g = MyGrammar("
+# Same as visit(v::NodeVisitor, node::Node) but here for debugging ...
+function visit(v::NodeVisitor, node::Node)
+    @show "rulevisitor", typeof(node)
+    visited_children = [visit(v, child) for child in node]
+    visit(v, node, visited_children)
+end
+
+# generic_visit - no children, just return node
+# except this overloads function visit(v::NodeVisitor, node::Node) and ruins everything
+#    @show "childless visit"
+#    pprettily(node)
+#    return node
+#end
+
+# remove after debug
+function visit(v::RuleVisitor, ::Node{:meaninglessness})
+    println("MEANINGLESSNESS")
+end
+
+# generic_visit - some children, just replace node with its children
+function visit(v::RuleVisitor, node::Node, visited_children...)
+    @show "generic parent visit"
+    pprettily(node)
+    return visited_children
+end
+
+function visit_quantifier(v::RuleVisitor, n::Node{:quantifier}, symbol, _)
+    @show "visit_quantifier", symbol
+    pprettily(node)
+    symbol
+end
+
+function visit_quantified(v::RuleVisitor, n::Node{:quantified}, atom, quantifier)
+    @show "visit_quantified"
+    pprettily(node)
+    if quantifier == '?'
+        return Optional(atom)
+    elseif quantifier == '*'
+        return ZeroOrMore(atom)
+    elseif quantifier == '+'
+        return OneOrMore(atom)
+    end
+    error("How is '" * quantifier * "' a quantifier to you?")
+end
+
+g = Grammar("""
             polite_greeting = greeting ', my good ' title
             greeting        = 'Hi' / 'Hello'
             title           = 'madam' / 'sir'
-            ")
+            """)
 _bootstrapping_expressions_from_rules()
 end
--- expressions.jl
+++ expressions.jl
@@ -3,18 +3,10 @@
 
 module Expressions
 
-# include("nodes.jl")
+import Base: match
+import Nodes: Node, isempty
 
-import Base: isempty, match
-using Nodes
-
-#function isempty(s::ASCIIString, t...)
-#    return isempty(s)
-#end
-
-abstract Expression
-#    name::ASCIIString
-
+abstract Expression # -ism
 
 type ParseError
     text::ASCIIString
@@ -22,13 +14,18 @@ type ParseError
     pos::Int64
 end
 
+function parse(expr::Expression, text::ASCIIString, pos::Int64=1)
+    node = match(expr, text, pos)
+    if node._end < length(text) + 1
+        error("Incomplete parse, stopped at char $(node._end) of $(pos): $(text[node._end:node._end+100])")
+    end
+    node
+end
 
 function match(expr::Expression, text::ASCIIString, pos::Int64=1)
-
     err = ParseError(text, expr, pos)
     node = _match!(expr, text, pos, Dict(), err)
-    # TODO: isempty(node::Node) -- how can I overload isempty and not have to call Nodes.isempty?
-    if Nodes.isempty(node)
+    if isempty(node)
         println("err.text: ", err.text)
         println("expr: ", expr)
         error(err.text)
@@ -46,8 +43,8 @@ function _match!(expr::Expression, text::ASCIIString, pos::Int64, cache::Dict, e
         cache[key] = node
     end
 
-    if Nodes.isempty(node) && pos >= err.pos && (
-        isempty(expr.name) || isempty(err.expr.name))
+    if isempty(node) && pos >= err.pos && (
+        Base.isempty(expr.name) || Base.isempty(err.expr.name))
         err.expr = expr
         err.pos = pos
     end
@@ -63,24 +60,22 @@ function as_rule(expr::Expression)
 end
 
 function _as_rhs(expr::Expression)
-    error("_as_rhs not implemented in Expression, only subclasses")
+    error("_as_rhs not implemented in "*string(typeof(expr))*" only subclasses")
 end
 
 type Literal <: Expression
-    name::ASCIIString
     literal::ASCIIString
-end
+    name::ASCIIString
 
-function Literal(literal::ASCIIString)
-    return Literal("", literal)
+    function Literal(literal::ASCIIString; name::ASCIIString="")
+        new(literal, name)
+    end
 end
 
 function _uncached_match(literal::Literal, text::ASCIIString, pos::Int64, cache::Dict, err::ParseError)
-    if beginswith(text, literal.literal)
-        println(text, " beginswith ", literal.literal)
+    if beginswith(text[pos:], literal.literal)
         return Node(literal.name, text, pos, pos + length(literal.literal))
     end
-    println(text, " does not beginswith ", literal.literal)
     Node()  # "Empty" node
 end
 
@@ -92,32 +87,26 @@ type Regex <: Expression
     name::ASCIIString
     re::Base.Regex
 
-    function Regex(pattern; name="", options="")
-        # TODO: Make pattern only match from pos by prepending '^' if it doesn't already have it.
+    function Regex(pattern; name="unnamed_regex", options="")
+        if pattern[1] != '^'
+            pattern = "^" * pattern
+        end
+
         new(name, Base.Regex(pattern, options))
     end
 end
 
 function _uncached_match(regex::Regex, text::ASCIIString, pos::Int64, cache::Dict, err::ParseError)
-    m = match(regex.re, text)
+    m = match(regex.re, text[pos:])
     if isa(m, Nothing)
         return Node()
     end
-    println("m.offset: ", m.offset, " pos: ", pos)
-    assert(m.offset == pos)  # should only match start of text
-    Node(regex.name, text, pos, pos + length(m.match))
+    assert(m.offset == 1)  # should only match start of text
+    Node(regex.name, text, pos, pos + length(m.match), match=m)
 end
 
 function _as_rhs(regex::Regex)
     "~\"$(re.pattern)\"$(re.options)"
-end
-
-function parse(expr::Expression, text::ASCIIString, pos::Int64=1)
-    node = match(expr, text, pos)
-    if node._end < length(text) + 1
-        error("Incomplete parse. TODO: better error reporting")
-    end
-    node
 end
 
 abstract _Compound <: Expression
@@ -138,7 +127,7 @@ function _uncached_match(sequence::Sequence, text::ASCIIString, pos::Int64, cach
     children = Node[]
     for m in sequence.members
         node = _match!(m, text, new_pos, cache, err)
-        if Nodes.isempty(node)
+        if isempty(node)
             return node
         end
         push!(children, node)
@@ -147,11 +136,11 @@ function _uncached_match(sequence::Sequence, text::ASCIIString, pos::Int64, cach
         length_of_sequence += length
     end
     # Hooray! We got through all the members!
-    return Node(sequence.name, text, pos, pos + length_of_sequence, children)
+    return Node(sequence.name, text, pos, pos + length_of_sequence, children=children)
 end
 
 function _as_rhs(sequence::Sequence)
-    return "TODO: return sequence"
+    return join(sequence.members, " ")
 end
 
 type OneOf <: _Compound
@@ -166,11 +155,15 @@ end
 function _uncached_match(oneof::OneOf, text::ASCIIString, pos::Int64, cache::Dict, err::ParseError)
     for m in oneof.members
         node = _match!(m, text, pos, cache, err)
-        if !Nodes.isempty(node)
-            return Node(oneof.name, text, pos, node._end, [node])
+        if !isempty(node)
+            return Node(oneof.name, text, pos, node._end, children=[node])
         end
     end
     return Node()
+end
+
+function _as_rhs(e::OneOf)
+    join(e.members, " / ")
 end
 
 type Lookahead <: _Compound
@@ -184,10 +177,14 @@ end
 
 function _uncached_match(self::Lookahead, text::ASCIIString, pos::Int64, cache::Dict, err::ParseError)
     node = _match!(self.members[1], text, pos, cache, err)
-    if !Nodes.isempty(node)
+    if !isempty(node)
         return Node(self.name, text, pos, pos)
     end
     return Node()
+end
+
+function _as_rhs(e::Lookahead)
+    "&" * join(e.members, " / ")
 end
 
 type Not <: _Compound
@@ -201,10 +198,14 @@ end
 
 function _uncached_match(self::Not, text::ASCIIString, pos::Int64, cache::Dict, err::ParseError)
     node = _match!(self.members[1], text, pos, cache, err)
-    if Nodes.isempty(node)
+    if isempty(node)
         return Node(self.name, text, pos, pos)
     end
     return Node()
+end
+
+function _as_rhs(e::Not)
+    "!" * join(e.members, " ")
 end
 
 type Optional <: _Compound
@@ -218,10 +219,14 @@ end
 
 function _uncached_match(self::Optional, text::ASCIIString, pos::Int64, cache::Dict, err::ParseError)
     node = _match!(self.members[1], text, pos, cache, err)
-    if Nodes.isempty(node)
+    if isempty(node)
         return Node(self.name, text, pos, pos)
     end
-    return Node(self.name, text, pos, node._end, [node])
+    return Node(self.name, text, pos, node._end, children=[node])
+end
+
+function _as_rhs(e::Optional)
+    join(e.members, " ") * "?"
 end
 
 type ZeroOrMore <: _Compound
@@ -238,13 +243,17 @@ function _uncached_match(self::ZeroOrMore, text::ASCIIString, pos::Int64, cache:
     children = Node[]
     while true
         node = _match!(self.members[1], text, new_pos, cache, err)
-        if Nodes.isempty(node) || (node._end - node.start) == 0
-            return Node(self.name, text, pos, new_pos, children)
+        if isempty(node) || (node._end - node.start) == 0
+            return Node(self.name, text, pos, new_pos, children=children)
         end
         push!(children, node)
         new_pos += node._end - node.start
     end
     return Node()
+end
+
+function _as_rhs(e::Optional)
+    join(e.members, " ") * "*"
 end
 
 type OneOrMore <: _Compound
@@ -262,7 +271,7 @@ function _uncached_match(self::OneOrMore, text::ASCIIString, pos::Int64, cache::
     children = Node[]
     while true
         node = _match!(self.members[1], text, new_pos, cache, err)
-        if Nodes.isempty(node)
+        if isempty(node)
             break
         end
         push!(children, node)
@@ -273,14 +282,18 @@ function _uncached_match(self::OneOrMore, text::ASCIIString, pos::Int64, cache::
         new_pos += len
     end
     if length(children) >= self._min
-        return Node(self.name, text, pos, new_pos, children)
+        return Node(self.name, text, pos, new_pos, children=children)
     end
     return Node()
 end
 
+function _as_rhs(e::Optional)
+    join(e.members, " ") * "+"
+end
+
 # Test
 
-l = Literal("foo", "foo")
+l = Literal("foo", name="foo")
 println(l)
 t = parse(l, "foo", 1)
 println(t)
