74094261ac2e9ba27550a0943341ebd1c7025117
Michael Fox <m@antikc.com>
2013-12-13 16:55:48 -0800

    Cleanup benchmarks

--- benchmarks.jl
+++ benchmarks.jl
@@ -1,36 +1,18 @@
-"""Benchmarks for Parsimonious
 
-Run these with ``nosetests parsimonious/tests/bench.py``. They don't run during
-normal test runs because they're not tests--they don't assert anything. Also,
-they're a bit slow.
-
-These differ from the ones in test_benchmarks in that these are meant to be
-compared from revision to revision of Parsimonious to make sure we're not
-getting slower. test_benchmarks simply makes sure our choices among
-implementation alternatives remain valid.
-
-"""
-# These aren't really tests, as they don't assert anything, but I found myself
-# rewriting nose's discovery and selection bits, so why not just use nose?
-
-module benchmarks
-
-# import gc -- have gc_disable(), gc_enable(), gc()
-# from timeit import repeat
+println("Julia is Alive")
+time_julia_alive = time()
 
 using Util
 using Grammars
-export parse, grammar, json
 
-# def test_not_really_json_parsing():
-"""As a baseline for speed, parse some JSON.
+REPEAT = 20  # Repeat parse several times and take minimum as benchmark
 
-I have no reason to believe that JSON is a particularly representative or
-revealing grammar to test with. Also, this is a naive, unoptimized,
-incorrect grammar, so don't use it as a basis for comparison with other
-parsers. It's just meant to compare across versions of Parsimonious.
-
-"""
+# As a baseline for speed, parse some JSON.
+# 
+# I have no reason to believe that JSON is a particularly representative or
+# revealing grammar to test with. Also, this is a naive, unoptimized,
+# incorrect grammar, so don't use it as a basis for comparison with other
+# parsers. It's just meant to compare across versions of Parsimonious.
 
 father = 
 """{
@@ -56,12 +38,9 @@ father =
 """
 more_fathers = join(repeat([father], inner=[60]), ",")
 
-json = """{"fathers" : [""" * more_fathers * "]}"
-# w = open("jljson.json", "w")
-# print(w, json)
-# close(w)
+sample_json = """{"fathers" : [""" * more_fathers * "]}"
 
-notjsonspec = """
+not_really_json_spec = """
     value = space (string / number / object / array / true_false_null)
             space
 
@@ -85,54 +64,36 @@ notjsonspec = """
     digit1to9 = ~"[1-9]"
     digit = ~"[0-9]"
     space = ~'\\s*'
-    """
+"""
 
-# notjsonspec = unescape_string(notjsonspec)
-println("SPEC:\n", notjsonspec)
-grammar = Grammar(notjsonspec)
+grammar = Grammar(not_really_json_spec)
 
-# This is what it takes to debug stupid quoting issues
-parse(grammar.exprs["chars"], """hello""")
-parse(grammar.exprs["number"], """12""")
-parse(grammar.exprs["dq"], "\"")
-parse(grammar.exprs["string"], (s""" "hello" """))
+# Compile
+parse(grammar, sample_json)
 
-# These number and repetition values seem to keep results within 5% of the
-# difference between min and max. We get more consistent results running a
-# bunch of single-parse tests and taking the min rather than upping the
-# NUMBER and trying to stomp out the outliers with averaging.
-NUMBER = 1
-REPEAT = 5
+time_compiled = time()
+elapsed_compiled = time_compiled - time_julia_alive
 
-parse(grammar, father)
-parse(grammar, json)
+@printf "Importing, compiling and initializing grammar done after %.2f s\n\n" elapsed_compiled
 
-# Disabling garbage collection during parse gets more consistent results.
-# The mean time doesn't seem to change much and is sometimes faster with gc
-# enabled somehow.
+# Disabling garbage collection (in Julia) during parse gets more consistent
+# results.  The mean time doesn't seem to change much and is sometimes faster
+# with gc enabled somehow. Anyway, let's not disable it so we can benchmark
+# more real-world.
 
 # gc_disable()
-# @timed -> (return vale, time in seconds, memory in bytes)
-min_time = minimum([@elapsed parse(grammar, json) for i in 1:5])
+min_time = minimum([@elapsed parse(grammar, sample_json) for i in 1:REPEAT])
 # gc_enable()
 # gc()
-@time parse(grammar, json)
 
-@printf "Min time to parse %.1f KB is %.0f ms which is %.0f KB/s\n" length(json)/1024. min_time*1e3 (length(json)/min_time/1024.)
+# Show time and memory allocated for one run. At the moment, should be around
+# 70 ms and 15 MB.
 
-# @timeit :(parse(grammar, json)) :() 1000
+println("For one pass over the text: @time parse(grammar, sample_json")
+@time parse(grammar, sample_json)
+println()
 
-end
-
-"""
-total_seconds = min(repeat(lambda: grammar.parse(json),
-                           lambda: gc.enable(),  # so we take into account how we treat the GC
-                           repeat=REPEAT,
-                           number=NUMBER))
-seconds_each = total_seconds / NUMBER
-
-kb = len(json) / 1024.0
-print 'Took %.3fs to parse %.1fKB: %.0fKB/s.' % (seconds_each,
-                                                 kb,
-                                                 kb / seconds_each)
-"""
+kb = length(sample_json)/1024.
+ms = min_time*1e3
+kbps =(length(sample_json)/min_time/1024.)
+@printf "Min time to parse %.1f KB is %.0f ms which is %.0f KB/s\n" kb ms kbps
